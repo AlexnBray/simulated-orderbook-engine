2b7c9ae48659f136dfb4dee7c57b6df954b0b3d6
AlexnBray <alexnbray585@gmail.com>
2026-06-09 10:02:54 +1000

    update partial update function

--- src/orders.cpp
+++ src/orders.cpp
@@ -4,6 +4,17 @@
 void Order::PartialUpdate(Quantity quantityfilled){
     //condition ? expression_if_true : expression_if_false;
     qty = (quantityfilled >= qty) ? 0 : (qty - quantityfilled);
+    if (qty == 0){
+        OrderBook.cancel(id)
+    }
 }
 
-void Order::modifyOrder();
+//if quantity = 0, delete the order
+
+void Order::modifyOrder(){
+
+
+}
+
+
+//streamlit
